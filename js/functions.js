6ba2ac7c74416ae0f91f02c86b7afdbe859a3e9b
Chris Yerkes <hello@mopsdigital.com>
2024-02-14 21:17:32 +0000

    Added in more content for each product link. Wrote CSS and JS that enabled the active product info to display when mouse hovered over nav item in mega menu

--- js/functions.js
+++ js/functions.js
@@ -27,4 +27,30 @@ document.addEventListener('DOMContentLoaded', function () {
 			}
 		});
 	});
+
+	// Select all links with a 'data-link' attribute
+	const cardLinks = document.querySelectorAll('a[data-link]');
+
+	cardLinks.forEach((link) => {
+		link.addEventListener('mouseover', function () {
+			// Find the currently active '.stop-gray-card' and remove the 'active' class
+			const currentActiveCard = document.querySelector(
+				'.stop-gray-card.active'
+			);
+			if (currentActiveCard) {
+				currentActiveCard.classList.remove('active');
+			}
+
+			// Get the 'data-link' value of the hovered link
+			const targetId = this.getAttribute('data-link');
+
+			// Find the '.stop-gray-card' that corresponds to the 'data-nav-target' and add 'active' class
+			const targetCard = document.querySelector(
+				`.stop-gray-card[data-nav-target="${targetId}"]`
+			);
+			if (targetCard) {
+				targetCard.classList.add('active');
+			}
+		});
+	});
 });
