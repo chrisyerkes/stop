992293f1c0d8d7efb87fed9b3e98ec4fcb1ff783
Chris Yerkes <hello@mopsdigital.com>
2024-03-07 21:15:06 +0000

    Finished desktop only bandaids. Testing in stoptheft.com environment.

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,5 @@
+/* Remove JS comments marked with 'REMOVE' before deploying to production */
+
 if ('ontouchstart' in window || navigator.maxTouchPoints > 0) {
 	console.log('The device is touch-enabled.');
 	// document.querySelector('html').classList.add('touch-enabled');
@@ -27,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	);
 	const navItems = document.querySelectorAll('.nav-item');
 
-	/* if (html.classList.contains('touch-enabled') || window.innerWidth < 992) {
+	/* REMOVE if (html.classList.contains('touch-enabled') || window.innerWidth < 992) {
 		navItems.forEach(function (navItem) {
 			if (navItem.querySelector('.sub-menu')) {
 				navItem.classList.add('has-child');
@@ -103,65 +105,48 @@ document.addEventListener('DOMContentLoaded', function () {
 				});
 			}
 		});
-	} */
+	} REMOVE */
 
 	// Desktop only behavior
-	if (window.innerWidth >= 992) {
-		primaryNavItems.forEach(function (navItem) {
-			navItem.addEventListener('mouseenter', function () {
-				const subMenu = navItem.querySelector(':scope > .sub-menu');
-				if (subMenu) {
-					const parentOffsetLeft = navItem.offsetLeft + 14;
-					subMenu.style.paddingLeft = `${parentOffsetLeft}px`;
-				}
-			});
+	/* REMOVE if (window.innerWidth >= 992) { REMOVE */
+	primaryNavItems.forEach(function (navItem) {
+		navItem.addEventListener('mouseenter', function () {
+			const subMenu = navItem.querySelector(':scope > .sub-menu');
+			if (subMenu) {
+				const parentOffsetLeft = navItem.offsetLeft + 14;
+				subMenu.style.paddingLeft = `${parentOffsetLeft}px`;
+			}
 		});
+	});
 
-		// Select all links with a 'data-link' attribute
-		const cardLinks = document.querySelectorAll('a[data-link]');
-		const navContainer = document.querySelector('#navbarNav'); // Ensure this selector matches your nav container
-		let originalActiveCard = document.querySelector('.card.active'); // Store the original active card
+	// Select all links with a 'data-link' attribute
+	const cardLinks = document.querySelectorAll('a[data-link]');
+	const navContainer = document.querySelector('#navbarNav'); // Ensure this selector matches your nav container
+	let originalActiveCard = document.querySelector('.card.active'); // Store the original active card
 
-		cardLinks.forEach((link) => {
-			link.addEventListener('mouseover', function () {
-				// Find the currently active '.card' and remove the 'active' class
-				const currentActiveCard =
-					document.querySelector('.card.active');
-				if (currentActiveCard) {
-					currentActiveCard.classList.remove('active');
-				}
+	cardLinks.forEach((link) => {
+		link.addEventListener('mouseover', function () {
+			// Find the currently active '.card' and remove the 'active' class
+			const currentActiveCard = document.querySelector('.card.active');
+			if (currentActiveCard) {
+				currentActiveCard.classList.remove('active');
+			}
 
-				// Get the 'data-link' value of the hovered link
-				const targetId = this.getAttribute('data-link');
+			// Get the 'data-link' value of the hovered link
+			const targetId = this.getAttribute('data-link');
 
-				// Find the '.card' that corresponds to the 'data-nav-target' and add 'active' class
-				const targetCard = document.querySelector(
-					`.card[data-nav-target="${targetId}"]`
-				);
-				if (targetCard) {
-					targetCard.classList.add('active');
-				}
+			// Find the '.card' that corresponds to the 'data-nav-target' and add 'active' class
+			const targetCard = document.querySelector(
+				`.card[data-nav-target="${targetId}"]`
+			);
+			if (targetCard) {
+				targetCard.classList.add('active');
+			}
 
-				// Update the originalActiveCard reference to the new active card
-				// originalActiveCard = document.querySelector('.card.active');
-			});
-			link.addEventListener('mouseleave', function () {
-				// Remove 'active' class from any currently active card
-				const currentActiveCard =
-					document.querySelector('.card.active');
-				if (currentActiveCard) {
-					currentActiveCard.classList.remove('active');
-				}
-
-				// Restore the 'active' class to the original active card
-				if (originalActiveCard) {
-					originalActiveCard.classList.add('active');
-				}
-			});
+			// Update the originalActiveCard reference to the new active card
+			// originalActiveCard = document.querySelector('.card.active');
 		});
-
-		// Mouseleave event to restore the original '.active' card when hovering off the nav
-		navContainer.addEventListener('mouseleave', function () {
+		link.addEventListener('mouseleave', function () {
 			// Remove 'active' class from any currently active card
 			const currentActiveCard = document.querySelector('.card.active');
 			if (currentActiveCard) {
@@ -173,7 +158,22 @@ document.addEventListener('DOMContentLoaded', function () {
 				originalActiveCard.classList.add('active');
 			}
 		});
-	}
+	});
+
+	// Mouseleave event to restore the original '.active' card when hovering off the nav
+	navContainer.addEventListener('mouseleave', function () {
+		// Remove 'active' class from any currently active card
+		const currentActiveCard = document.querySelector('.card.active');
+		if (currentActiveCard) {
+			currentActiveCard.classList.remove('active');
+		}
+
+		// Restore the 'active' class to the original active card
+		if (originalActiveCard) {
+			originalActiveCard.classList.add('active');
+		}
+	});
+	/* REMOVE } REMOVE */
 
 	// Footer Nav Dropdowns
 	const footerNavItems = document.querySelectorAll(
